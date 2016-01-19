9aef9af48efd6bf8fe03cd39e54b90bc8f929785
Jeevanandam M <jeeva@myjeeva.com>
2016-01-19 14:15:49 -0800

    Copy method is done

--- model.go
+++ model.go
@@ -17,20 +17,20 @@ import (
 const (
 	TagName    = "model"
 	OmitField  = "-"
-	OmitNested = "omitnested"
+	NoTraverse = "notraverse"
 )
 
 var (
-	Version            = "0.1"
-	omitNestedTypeList map[reflect.Type]bool
+	Version            = "0.1-beta"
+	NoTraverseTypeList map[reflect.Type]bool
 )
 
 func init() {
-	omitNestedTypeList = map[reflect.Type]bool{}
+	NoTraverseTypeList = map[reflect.Type]bool{}
 
-	// Auto omit nested struct list for not traversing DeepLevel
-	// However, variable value will be evaluated
-	AddToOmitNested(
+	// Auto No Traverse struct list for not traversing DeepLevel
+	// However, attribute value will be evaluated by go-model
+	AddToNoTraverseList(
 		time.Time{},
 		&time.Time{},
 		os.File{},
@@ -42,14 +42,15 @@ func init() {
 	)
 }
 
-func AddToOmitNested(i ...interface{}) {
+func AddToNoTraverseList(i ...interface{}) {
 	for _, v := range i {
 		t := typeOf(v)
-		if _, ok := omitNestedTypeList[t]; ok {
+		if _, ok := NoTraverseTypeList[t]; ok {
+			// already registered for no traverse, move on
 			continue
 		}
 
-		omitNestedTypeList[t] = true
+		NoTraverseTypeList[t] = true
 	}
 }
 
@@ -58,28 +59,29 @@ func IsZero(s interface{}) bool {
 		return true
 	}
 
-	sv := valueOf(s)
+	sv := indirect(valueOf(s))
 	fields := Fields(sv)
-	sv = indirect(sv)
 
 	for _, f := range fields {
 		fv := sv.FieldByName(f.Name)
 
 		// embeded or nested struct
 		if isStruct(fv) {
-			if isOmitNestedType(fv) || isOmitNested(f.Tag.Get(TagName)) {
+			if isNoTraverseType(fv) || isNoTraverse(f.Tag.Get(TagName)) {
+
+				// not traversing inside, but evaluating a value
 				if !IsFieldZero(fv) {
 					return false
 				}
 
-				continue
+				continue // move on to next attribute
 			}
 
 			if !IsZero(fv.Interface()) {
 				return false
 			}
 
-			continue
+			continue // move on to next attribute
 		}
 
 		if !IsFieldZero(fv) {
@@ -92,7 +94,7 @@ func IsZero(s interface{}) bool {
 
 func IsFieldZero(f reflect.Value) bool {
 	// zero value of the given field
-	// For example: Zero(TypeOf(42)) returns a Value with Kind Int and value 0
+	// For example: reflect.Zero(reflect.TypeOf(42)) returns a Value with Kind Int and value 0
 	zero := reflect.Zero(f.Type()).Interface()
 
 	return reflect.DeepEqual(f.Interface(), zero)
@@ -124,18 +126,18 @@ func Fields(v reflect.Value) []reflect.StructField {
 func Copy(dst, src interface{}, zero bool) []error {
 	var errs []error
 
-	if IsZero(src) {
-		return append(errs, errors.New("Source struct is empty"))
+	sv := valueOf(src)
+	dv := valueOf(dst)
+	if !isStruct(sv) || !isStruct(dv) {
+		return append(errs, errors.New("Source or Destination is not a struct"))
 	}
 
-	dv := valueOf(dst)
 	if !isPtr(dv) {
 		return append(errs, errors.New("Destination struct is not a pointer"))
 	}
 
-	sv := valueOf(src)
-	if !isStruct(sv) || !isStruct(dv) {
-		return append(errs, errors.New("Source or Destination is not a struct"))
+	if IsZero(src) {
+		return append(errs, errors.New("Source struct is empty"))
 	}
 
 	// processing copy value(s)
@@ -169,13 +171,13 @@ func doCopy(dv, sv reflect.Value, zero bool) []error {
 
 			// check dst field is exists, if not valid move on
 			if !dfv.IsValid() {
-				errs = append(errs, fmt.Errorf("Field: %v, dst is not valid", f.Name))
+				errs = append(errs, fmt.Errorf("Field: '%v', dst is not valid", f.Name))
 				continue
 			}
 
 			// check kind of src and dst, if doesn't match move on
 			if sfv.Kind() != dfv.Kind() {
-				errs = append(errs, fmt.Errorf("Field: %v, src & dst is different kind", f.Name))
+				errs = append(errs, fmt.Errorf("Field: '%v', src & dst kind doesn't match", f.Name))
 				continue
 			}
 
@@ -192,13 +194,14 @@ func doCopy(dv, sv reflect.Value, zero bool) []error {
 					continue // move on to next attribute
 				}
 
+				// handle embeded/nested struct
 				if isStruct(sfv) {
 					fmt.Println("This is struct kind:", typeOf(sfv))
-					if isOmitNestedType(sfv) || isOmitNested(f.Tag.Get(TagName)) {
+					if isNoTraverseType(sfv) || isNoTraverse(f.Tag.Get(TagName)) {
 						fmt.Println("We are not going to traverse")
 						// This is struct kind, but we are not going to traverse
-						// since its in omitNestedTypeList
-						// however we will take care of current value
+						// since its in NoTraverseTypeList
+						// however we will take care of attribute value
 						dfv.Set(val(sfv))
 					} else {
 						fmt.Println("We are going to traverse inside")
@@ -209,7 +212,7 @@ func doCopy(dv, sv reflect.Value, zero bool) []error {
 							errs = append(errs, innerErrs...)
 						}
 
-						// handle based value ptr/non-ptr
+						// handle based on ptr/non-ptr value
 						if isPtr(sfv) {
 							dfv.Set(ndv)
 						} else {
@@ -223,18 +226,17 @@ func doCopy(dv, sv reflect.Value, zero bool) []error {
 				dfv.Set(val(sfv))
 			}
 		}
-
 	}
 
-	return nil
+	return errs
 }
 
 // Non-exported methods of model library
 
-func isOmitNestedType(v reflect.Value) bool {
+func isNoTraverseType(v reflect.Value) bool {
 	t := indirect(v).Type()
 
-	if _, ok := omitNestedTypeList[t]; ok {
+	if _, ok := NoTraverseTypeList[t]; ok {
 		return true
 	}
 
@@ -242,6 +244,12 @@ func isOmitNestedType(v reflect.Value) bool {
 }
 
 func val(f reflect.Value) reflect.Value {
+	// take care interface{} and its actual value
+	if isInterface(f) {
+		f = valueOf(f.Interface())
+	}
+
+	// handling pointer value
 	if isPtr(f) {
 		fmt.Println("Value is Ptr:", f.Interface(), f.Elem().Interface())
 
@@ -251,41 +259,41 @@ func val(f reflect.Value) reflect.Value {
 		return nf
 	}
 
+	// handling non-pointer value
 	fmt.Println("Value is not a Ptr:", f.Interface())
-	// regular attribute may hold pointer
-	// for eg.: map, slice
+	// regular attribute may hold pointer reference for eg.: map, slice
+	// typically its a interface{} scenario
 	switch f.Kind() {
 	case reflect.Map:
-		fmt.Println("This is map kind: ", len(f.MapKeys()))
-		if len(f.MapKeys()) > 0 {
-			fv := f.MapIndex(f.MapKeys()[0])
-			fmt.Println("map first value is:", fv.Interface())
-			if isPtr(fv) {
-				fmt.Println("Found pointer element, start processing")
+		fmt.Println("Map value count:", f.Len())
+		if f.Len() > 0 {
+			nf := reflect.MakeMap(f.Type())
 
-				nf := reflect.MakeMap(f.Type())
-				for _, key := range f.MapKeys() {
-					ov := f.MapIndex(key)
-					cv := reflect.New(ov.Type()).Elem()
-					cv.Set(val(ov))
-					nf.SetMapIndex(key, cv)
-				}
-
-				return nf
+			for _, key := range f.MapKeys() {
+				// getting actual map value
+				ov := f.MapIndex(key)
+				ovt := ov.Type()
+				cv := reflect.New(ovt).Elem()
+				cv.Set(val(ov))
+				nf.SetMapIndex(key, cv)
 			}
+
+			return nf
 		}
 	case reflect.Slice:
+		fmt.Println("Slice value count:", f.Len())
 		if f.Len() > 0 {
-			// handle if slice element is pointer else return as-is
-			if isPtr(f.Index(0)) {
-				nf := reflect.MakeSlice(f.Type(), f.Len(), f.Cap())
+			nf := reflect.MakeSlice(f.Type(), f.Len(), f.Cap())
 
-				for i := 0; i < f.Len(); i++ {
-					nf.Index(i).Set(val(f.Index(i)))
-				}
-
-				return indirect(nf)
+			for i := 0; i < f.Len(); i++ {
+				ov := f.Index(i)
+				ovt := ov.Type()
+				cv := reflect.New(ovt).Elem()
+				cv.Set(val(ov))
+				nf.Index(i).Set(cv)
 			}
+
+			return indirect(nf)
 		}
 	}
 
@@ -304,12 +312,12 @@ func zeroVal(f reflect.Value) reflect.Value {
 	return indirect(valueOf(ftz.Interface()))
 }
 
-func isOmitNested(tag string) bool {
-	return strings.Contains(tag, OmitNested)
+func isNoTraverse(tag string) bool {
+	return strings.Contains(tag, NoTraverse)
 }
 
 func typeOf(i interface{}) reflect.Type {
-	return reflect.TypeOf(i) // indirect(valueOf(i)).Type()
+	return reflect.TypeOf(i)
 }
 
 func valueOf(i interface{}) reflect.Value {
@@ -326,4 +334,8 @@ func isPtr(v reflect.Value) bool {
 
 func isStruct(v reflect.Value) bool {
 	return indirect(v).Kind() == reflect.Struct
+}
+
+func isInterface(v reflect.Value) bool {
+	return v.Kind() == reflect.Interface
 }
--- model_test.go
+++ model_test.go
@@ -56,16 +56,340 @@ type SubInfoDeep struct {
 	SubInfo
 }
 
-func TestAddOmitNested(t *testing.T) {
-	if !isOmitNestedType(valueOf(os.File{})) {
+func TestCopyIntegerAndIntegerPtr(t *testing.T) {
+	type SampleStruct struct {
+		Int      int
+		IntPtr   *int
+		Int64    int64
+		Int64Ptr *int64
+	}
+
+	intPtr := int(1001)
+	int64Ptr := int64(1002)
+
+	src := SampleStruct{
+		Int:      2001,
+		IntPtr:   &intPtr,
+		Int64:    2002,
+		Int64Ptr: &int64Ptr,
+	}
+
+	dst := SampleStruct{}
+
+	errs := Copy(&dst, src, false)
+	if errs != nil {
+		t.Error("Error occurred while copying.")
+	}
+
+	logSrcDst(t, src, dst)
+
+	assertEqual(t, src.Int, dst.Int)
+	assertEqual(t, src.Int64, dst.Int64)
+
+	assertEqual(t, true, src.IntPtr != dst.IntPtr)
+	assertEqual(t, *src.IntPtr, *dst.IntPtr)
+	assertEqual(t, *src.Int64Ptr, *dst.Int64Ptr)
+}
+
+func TestCopyStringAndStringPtr(t *testing.T) {
+	type SampleStruct struct {
+		String    string
+		StringPtr *string
+	}
+
+	strPtr := "This is string for pointer test"
+	src := SampleStruct{
+		String:    "This is string for test",
+		StringPtr: &strPtr,
+	}
+
+	dst := SampleStruct{}
+
+	errs := Copy(&dst, &src, false)
+	if errs != nil {
+		t.Error("Error occurred while copying.")
+	}
+
+	logSrcDst(t, src, dst)
+
+	assertEqual(t, src.String, dst.String)
+	assertEqual(t, *src.StringPtr, *dst.StringPtr)
+	assertEqual(t, true, src.StringPtr != dst.StringPtr)
+}
+
+//
+// TODO for Boolean, float, etc
+//
+
+func TestCopySliceStringAndSliceStringPtr(t *testing.T) {
+	type SampleStruct struct {
+		SliceString    []string
+		SliceStringPtr *[]string
+	}
+
+	sliceStrPtr := []string{"This is slice string test pointer."}
+	src := SampleStruct{
+		SliceString:    []string{"This is slice string test."},
+		SliceStringPtr: &sliceStrPtr,
+	}
+
+	dst := SampleStruct{}
+
+	errs := Copy(&dst, &src, false)
+	if errs != nil {
+		t.Error("Error occurred while copying.")
+	}
+
+	logSrcDst(t, src, dst)
+
+	assertEqual(t, src.SliceString, dst.SliceString)
+	assertEqual(t, *src.SliceStringPtr, *dst.SliceStringPtr)
+	assertEqual(t, true, src.SliceStringPtr != dst.SliceStringPtr)
+}
+
+func TestCopySliceElementsPtr(t *testing.T) {
+	type SampleStruct struct {
+		SliceIntPtr    []*int
+		SliceInt64Ptr  []*int64
+		SliceStringPtr []*string
+		SliceFloat32   []*float32
+		SliceFloat64   []*float64
+	}
+
+	i1 := int(1)
+	i2 := int(2)
+	i3 := int(3)
+
+	i11 := int64(11)
+	i12 := int64(12)
+	i13 := int64(13)
+
+	str1 := "This is string pointer 1"
+	str2 := "This is string pointer 2"
+	str3 := "This is string pointer 3"
+
+	f1 := float32(0.1)
+	f2 := float32(0.2)
+	f3 := float32(0.3)
+
+	f11 := float64(0.11)
+	f12 := float64(0.12)
+	f13 := float64(0.13)
+
+	src := SampleStruct{
+		SliceIntPtr:    []*int{&i1, &i2, &i3},
+		SliceInt64Ptr:  []*int64{&i11, &i12, &i13},
+		SliceStringPtr: []*string{&str1, &str2, &str3},
+		SliceFloat32:   []*float32{&f1, &f2, &f3},
+		SliceFloat64:   []*float64{&f11, &f12, &f13},
+	}
+
+	dst := SampleStruct{}
+
+	errs := Copy(&dst, src, false)
+	if errs != nil {
+		t.Error("Error occurred while copying.")
+	}
+
+	logSrcDst(t, src, dst)
+
+	assertEqual(t, src.SliceIntPtr, dst.SliceIntPtr)
+	assertEqual(t, src.SliceInt64Ptr, dst.SliceInt64Ptr)
+	assertEqual(t, src.SliceStringPtr, dst.SliceStringPtr)
+	assertEqual(t, src.SliceFloat32, dst.SliceFloat32)
+	assertEqual(t, src.SliceFloat64, dst.SliceFloat64)
+}
+
+//
+// TODO slice with interface{}, etc.
+//
+
+func TestCopyMapElements(t *testing.T) {
+	type SampleSubInfo struct {
+		Name string
+		Year int
+	}
+
+	type SampleStruct struct {
+		MapIntInt       map[int]int
+		MapStringInt    map[string]int
+		MapStringString map[string]string
+		MapStruct       map[string]SubInfo
+		MapInterfaces   map[string]interface{}
+	}
+
+	src := SampleStruct{
+		MapIntInt:       map[int]int{1: 1001, 2: 1002, 3: 1003, 4: 1004},
+		MapStringInt:    map[string]int{"first": 1001, "second": 1002, "third": 1003, "forth": 1004},
+		MapStringString: map[string]string{"first": "1001", "second": "1002", "third": "1003"},
+		MapStruct: map[string]SubInfo{
+			"struct1": SubInfo{Name: "struct 1 value", Year: 2001},
+			"struct2": SubInfo{Name: "struct 2 value", Year: 2002},
+			"struct3": SubInfo{Name: "struct 3 value", Year: 2003},
+		},
+		MapInterfaces: map[string]interface{}{
+			"inter1": 100001,
+			"inter2": "This is my interface string",
+			"inter3": SubInfo{Name: "struct 3 value", Year: 2003},
+			"inter4": float32(1.6546565),
+			"inter5": float64(1.6546565),
+			"inter6": &SubInfo{Name: "struct 3 value", Year: 2006},
+		},
+	}
+
+	dst := SampleStruct{}
+
+	errs := Copy(&dst, &src, false)
+	if errs != nil {
+		t.Error("Error occurred while copying.")
+	}
+
+	logSrcDst(t, src, dst)
+
+	assertEqual(t, src.MapIntInt, dst.MapIntInt)
+	assertEqual(t, src.MapStringInt, dst.MapStringInt)
+	assertEqual(t, src.MapStringString, dst.MapStringString)
+	assertEqual(t, src.MapStruct, dst.MapStruct)
+	assertEqual(t, src.MapInterfaces, dst.MapInterfaces)
+}
+
+func TestCopyStructEmbededAndAttribute(t *testing.T) {
+	type SampleSubInfo struct {
+		Name string
+		Year int
+	}
+
+	type SampleStruct struct {
+		SampleSubInfo
+		Level1Struct     SampleSubInfo
+		Level1StructPtr  *SampleSubInfo
+		Level1StructOmit *SampleSubInfo `model:",omitnested"`
+		CreatedTime      time.Time
+	}
+
+	src := SampleStruct{
+		SampleSubInfo:    SampleSubInfo{Name: "This embeded struct", Year: 2016},
+		Level1Struct:     SampleSubInfo{Name: "This level 1 struct", Year: 2015},
+		Level1StructPtr:  &SampleSubInfo{Name: "This level 2 struct", Year: 2014},
+		Level1StructOmit: &SampleSubInfo{Name: "This omit nested traverse struct", Year: 2013},
+		CreatedTime:      time.Now(),
+	}
+
+	dst := SampleStruct{}
+
+	errs := Copy(&dst, &src, false)
+	if errs != nil {
+		t.Error("Error occurred while copying.")
+	}
+
+	logSrcDst(t, src, dst)
+
+	assertEqual(t, src.Name, dst.Name)
+	assertEqual(t, src.Year, dst.Year)
+
+	assertEqual(t, src.Level1Struct.Name, dst.Level1Struct.Name)
+	assertEqual(t, src.Level1Struct.Year, dst.Level1Struct.Year)
+
+	assertEqual(t, src.Level1StructPtr.Name, dst.Level1StructPtr.Name)
+	assertEqual(t, src.Level1StructPtr.Year, dst.Level1StructPtr.Year)
+
+	assertEqual(t, true, src.CreatedTime == dst.CreatedTime)
+	assertEqual(t, src.Level1StructOmit.Year, dst.Level1StructOmit.Year)
+}
+
+func TestCopyZeroInput(t *testing.T) {
+	errs := Copy(&SampleStruct{}, SampleStruct{}, false)
+
+	assertEqual(t, "Source struct is empty", errs[0].Error())
+}
+
+func TestCopyDestinationIsNotPointer(t *testing.T) {
+	type SampleStruct struct {
+		Name string
+	}
+	errs := Copy(SampleStruct{}, SampleStruct{Name: "Not a pointer"}, false)
+
+	assertEqual(t, "Destination struct is not a pointer", errs[0].Error())
+}
+
+func TestCopyInputIsNotStruct(t *testing.T) {
+	type SampleStruct struct {
+		Name string
+	}
+	errs := Copy(&SampleStruct{}, map[string]string{"1": "2001"}, false)
+
+	assertEqual(t, "Source or Destination is not a struct", errs[0].Error())
+}
+
+func TestCopyStructElementKindDiff(t *testing.T) {
+	type Source struct {
+		Name string
+	}
+
+	type Destination struct {
+		Name int
+	}
+
+	errs := Copy(&Destination{}, Source{Name: "This struct element kind is different"}, false)
+
+	assertEqual(t, "Field: 'Name', src & dst kind doesn't match", errs[0].Error())
+}
+
+func TestCopyStructElementIsNotValidInDst(t *testing.T) {
+	type Source struct {
+		Name string
+		Year int
+	}
+
+	type Destination struct {
+		Name string
+	}
+
+	src := Source{Year: 2016}
+	dst := Destination{Name: "Value is gonna disappear"}
+
+	errs := Copy(&dst, src, true)
+
+	assertEqual(t, "Field: 'Year', dst is not valid", errs[0].Error())
+}
+
+func TestCopyStructZeroValToDst(t *testing.T) {
+	type Source struct {
+		Name string
+		Year int
+	}
+
+	type Destination struct {
+		Name string
+		Year int
+	}
+
+	src := Source{Year: 2016}
+	dst := Destination{Name: "Value is gonna disappear"}
+
+	errs := Copy(&dst, src, true)
+	if errs != nil {
+		t.Error("Error occurred while copying.")
+	}
+
+	logSrcDst(t, src, dst)
+
+	assertEqual(t, "", dst.Name)
+	assertEqual(t, 2016, dst.Year)
+}
+
+func TestAddToNoTraverselist(t *testing.T) {
+	if !isNoTraverseType(valueOf(os.File{})) {
 		t.Errorf("Given type not found in omit list")
 	}
 
 	// Already registered
-	AddToOmitNested(os.File{})
+	AddToNoTraverseList(os.File{})
 }
 
 func TestIsZero(t *testing.T) {
+	IsZero(nil) // nil check
+
 	if !IsZero(SampleStruct{}) {
 		t.Error("SampleStruct - supposed to be zero")
 	}
@@ -107,74 +431,6 @@ func TestNonZeroCheck(t *testing.T) {
 	if IsZero(SampleStruct{SubInfo: SubInfo{Year: 2010}}) {
 		t.Error("SampleStruct embeded struct - supposed to be non-zero")
 	}
-}
-
-func TestCopyOnlyNonZeroPtr(t *testing.T) {
-	intPtr := 1002
-	deepIntPtr := 2006
-	deepNamePtr := "Deep: go-model struct ptr"
-	src := SampleStruct{
-		Integer:    1001,
-		IntegerPtr: &intPtr,
-		Struct:     SubInfo{Name: "go-model struct", Year: 2014},
-		StructPtr:  &SubInfo{Name: "go-model struct ptr", Year: 2015},
-		StructDeep: SubInfoDeep{
-			Name:      "Deep: go-model struct",
-			Year:      1996,
-			NamePtr:   &deepNamePtr,
-			YearPtr:   &deepIntPtr,
-			Struct:    SubInfo{Name: "Deep: go-model struct", Year: 1994},
-			StructPtr: &SubInfo{Name: "Deep: go-model struct ptr", Year: 1995},
-			SubInfo:   SubInfo{Name: "Deep: go-model embeded", Year: 1996},
-		},
-		StructDeepPtr: &SubInfoDeep{
-			Name:      "Deep: go-model struct ptr",
-			Year:      2006,
-			NamePtr:   &deepNamePtr,
-			YearPtr:   &deepIntPtr,
-			Struct:    SubInfo{Name: "Deep: go-model struct", Year: 2004},
-			StructPtr: &SubInfo{Name: "Deep: go-model struct ptr", Year: 2005},
-			SubInfo:   SubInfo{Name: "Deep: go-model embeded", Year: 2006},
-		},
-		SubInfo: SubInfo{Name: "go-model embeded", Year: 2016},
-	}
-
-	dst := SampleStruct{}
-
-	errs := Copy(&dst, &src, false)
-	fmt.Println("Errors:", errs)
-
-	fmt.Printf("\nSource     : %#v\n", src)
-	fmt.Printf("\nDestination: %#v\n", dst)
-	fmt.Println()
-
-	assertEqual(t, src.Integer, dst.Integer)
-	assertEqual(t, true, src.IntegerPtr != dst.IntegerPtr)
-	assertEqual(t, *src.IntegerPtr, *dst.IntegerPtr)
-
-	// Level 1 struct Assertion
-	assertEqual(t, src.Struct.Name, dst.Struct.Name)
-	assertEqual(t, src.Struct.Year, dst.Struct.Year)
-
-	assertEqual(t, true, src.StructPtr != dst.StructPtr)
-	assertEqual(t, src.StructPtr.Name, dst.StructPtr.Name)
-	assertEqual(t, src.StructPtr.Year, dst.StructPtr.Year)
-
-	// Level 2 strcut Assertion
-	assertEqual(t, 0, dst.StructDeep.Year)
-	assertEqual(t, true, dst.StructDeep.NamePtr == nil)
-	assertEqual(t, src.StructDeep.Struct.Name, dst.StructDeep.Struct.Name)
-	assertEqual(t, true, src.StructDeep.StructPtr != dst.StructDeep.StructPtr)
-	assertEqual(t, src.StructDeep.StructPtr.Year, dst.StructDeep.StructPtr.Year)
-	assertEqual(t, src.StructDeep.SubInfo.Year, dst.StructDeep.SubInfo.Year)
-
-	assertEqual(t, 0, dst.StructDeepPtr.Year)
-	assertEqual(t, true, dst.StructDeepPtr.NamePtr == nil)
-	assertEqual(t, true, src.StructDeepPtr != dst.StructDeepPtr)
-	assertEqual(t, src.StructDeepPtr.Struct.Name, dst.StructDeepPtr.Struct.Name)
-	assertEqual(t, true, src.StructDeepPtr.StructPtr != dst.StructDeepPtr.StructPtr)
-	assertEqual(t, src.StructDeepPtr.StructPtr.Year, dst.StructDeepPtr.StructPtr.Year)
-	assertEqual(t, src.StructDeepPtr.SubInfo.Year, dst.StructDeepPtr.SubInfo.Year)
 }
 
 func assertError(t *testing.T, err error) {
@@ -221,7 +477,19 @@ func compare(e, g interface{}) (r bool) {
 		r = (ev.String() == gv.String())
 	case reflect.Bool:
 		r = (ev.Bool() == gv.Bool())
+	case reflect.Slice, reflect.Map:
+		r = reflect.DeepEqual(e, g)
 	}
 
 	return
+}
+
+func logSrcDst(t *testing.T, src, dst interface{}) {
+	logIt(t, "Source", src)
+	fmt.Println()
+	logIt(t, "Destination", dst)
+}
+
+func logIt(t *testing.T, str string, v interface{}) {
+	t.Logf("%v: %#v", str, v)
 }
