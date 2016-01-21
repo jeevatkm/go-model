aee43c5848b95a9c80cc83994fd20a5eab3e2d20
Jeevanandam M <jeeva@myjeeva.com>
2016-01-21 14:13:52 -0800

    model tag parsing and option methods

---
+++ tag.go
@@ -0,0 +1,38 @@
+// Copyright (c) 2016 Jeevanandam M (jeeva@myjeeva.com), All rights reserved.
+// resty source code and usage is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package model
+
+import "strings"
+
+type tag struct {
+	Name    string
+	Options string
+}
+
+func newTag(modelTag string) *tag {
+	t := tag{}
+	values := strings.Split(modelTag, ",")
+
+	t.Name = values[0]
+	t.Options = strings.Join(values[1:], ",")
+
+	return &t
+}
+
+func (t *tag) isOmitEmpty() bool {
+	return t.isExists(OmitEmpty)
+}
+
+func (t *tag) isNoTraverse() bool {
+	return t.isExists(NoTraverse)
+}
+
+func (t *tag) isExists(opt string) bool {
+	return strings.Contains(t.Options, opt)
+}
+
+func isStringEmpty(str string) bool {
+	return (len(strings.TrimSpace(str)) == 0)
+}
---
+++ tag_test.go
@@ -0,0 +1,83 @@
+package model
+
+import "testing"
+
+func TestNewTag(t *testing.T) {
+	tag := newTag("fieldName,omitempty,notraverse")
+
+	logIt(t, "Model Tag", tag)
+
+	assertEqual(t, "fieldName", tag.Name)
+	assertEqual(t, "omitempty,notraverse", tag.Options)
+}
+
+func TestNewTagWithoutName(t *testing.T) {
+	tag := newTag(",omitempty,notraverse")
+
+	logIt(t, "Model Tag", tag)
+
+	assertEqual(t, "", tag.Name)
+	assertEqual(t, "omitempty,notraverse", tag.Options)
+}
+
+func TestNewTagNoValues(t *testing.T) {
+	tag := newTag("")
+
+	logIt(t, "Model Tag", tag)
+
+	assertEqual(t, "", tag.Name)
+	assertEqual(t, "", tag.Options)
+}
+
+func TestNewTagEmptyValues(t *testing.T) {
+	tag := newTag(",")
+
+	logIt(t, "Model Tag", tag)
+
+	assertEqual(t, "", tag.Name)
+	assertEqual(t, "", tag.Options)
+}
+
+func TestIsOmitEmpty(t *testing.T) {
+	tag1 := newTag("fieldName,omitempty,notraverse")
+	logIt(t, "Model Tag", tag1)
+	assertEqual(t, true, tag1.isOmitEmpty())
+
+	tag2 := newTag(",omitempty")
+	logIt(t, "Model Tag", tag2)
+	assertEqual(t, true, tag2.isOmitEmpty())
+
+	tag3 := newTag(",omitempty,notraverse")
+	logIt(t, "Model Tag", tag3)
+	assertEqual(t, true, tag3.isOmitEmpty())
+
+	tag4 := newTag(",notraverse")
+	logIt(t, "Model Tag", tag4)
+	assertEqual(t, false, tag4.isOmitEmpty())
+
+	tag5 := newTag("fieldName")
+	logIt(t, "Model Tag", tag5)
+	assertEqual(t, false, tag5.isOmitEmpty())
+}
+
+func TestIsNoTraverse(t *testing.T) {
+	tag1 := newTag("fieldName,omitempty,notraverse")
+	logIt(t, "Model Tag", tag1)
+	assertEqual(t, true, tag1.isNoTraverse())
+
+	tag2 := newTag(",notraverse")
+	logIt(t, "Model Tag", tag2)
+	assertEqual(t, true, tag2.isNoTraverse())
+
+	tag3 := newTag(",omitempty,notraverse")
+	logIt(t, "Model Tag", tag3)
+	assertEqual(t, true, tag3.isNoTraverse())
+
+	tag4 := newTag(",omitempty")
+	logIt(t, "Model Tag", tag4)
+	assertEqual(t, false, tag4.isNoTraverse())
+
+	tag5 := newTag("fieldName")
+	logIt(t, "Model Tag", tag5)
+	assertEqual(t, false, tag5.isNoTraverse())
+}
